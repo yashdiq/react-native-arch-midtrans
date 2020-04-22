acd8260538ffbf498297e67c3c114ca7f3ad985c
Yashdiq Lubis <yasdiq@teknotera.com>
2020-04-22 12:30:58 +0700

    Updated

--- index.js
+++ index.js
@@ -2,4 +2,17 @@ import { NativeModules } from 'react-native';
 
 const { ArchMidtrans } = NativeModules;
 
-export default ArchMidtrans;
+export default {
+  checkOut: function (optionConect, transRequest, itemDetails, creditCardOptions, mapUserDetail,
+    optionColorTheme, optionFont, resultCheckOut) {
+    ArchMidtrans.checkOut(
+    optionConect,
+    transRequest,
+    itemDetails,
+    creditCardOptions,
+    mapUserDetail,
+    optionColorTheme,
+    optionFont,
+    resultCheckOut);
+  },
+};
